2d0389d890ef3de75b242de66628df5850c04357
Koshechkin Vadim <rizus@yandex.ru>
2022-12-16 03:43:26 +0300

    remove unnecessary files

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,10 @@
+// NOTE(rizus): left an example for the future
+
+/**
 import { expect, test } from '@playwright/test';
 
 test('about page has expected h1', async ({ page }) => {
 	await page.goto('/about');
 	expect(await page.textContent('h1')).toBe('About this app');
 });
+ */
