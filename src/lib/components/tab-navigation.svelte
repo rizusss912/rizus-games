1f0839a51abb0c368ccf3e124f154e2b896992f4
rizusss912 <rizus912@yandex.ru>
2022-12-31 00:30:57 +0300

    fix tab navigation

--- src/lib/components/tab-navigation.svelte
+++ src/lib/components/tab-navigation.svelte
@@ -1,5 +1,32 @@
+<script lang="ts">
+    const TAB_NAVIGATION_CLASS = 'tabnav';
+    const TAB_KEY = 'Tab';
+  
+    function clickHandler() {
+      document.body.classList.remove(TAB_NAVIGATION_CLASS);
+    }
+
+    function keyDownHandler({ key }: KeyboardEvent) {
+      if (key !== TAB_KEY) {
+        return;
+      }
+
+      document.body.classList.add(TAB_NAVIGATION_CLASS);
+    }
+</script>
+  
+<svelte:body on:mousedown={clickHandler} on:keydown={keyDownHandler} />
+
 <style global>
+    :global(body.tabnav) :global(*):focus {
+        box-shadow: 0 0 0 2px var(--background-color), 0 0 0 4px var(--primary-color);
+    }
+
     :global(*):focus {
-        outline: 1px solid rgb(171, 9, 9);
+        outline: none;
+    }
+
+    :global(*)::-moz-focus-inner {
+        border: 0;
     }
 </style>
